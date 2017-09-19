05ca3526e1794f1155594f67c0c5fdf17b78d7e6
AlyssonGGS <Alyssonggs@hotmail.com>
2017-09-19 20:35:50 -0300

    Menu criado

---
+++ WAR/Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+	public string redirectSceneName;
+	public void changeSceneOnClick(){
+		SceneManager.LoadScene(redirectSceneName);
+	}
+}
