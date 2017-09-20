ec9d46d53c9cd5ae83f1a82f2b72e9d190fb30c0
AlyssonGGS <Alyssonggs@hotmail.com>
2017-09-19 21:17:06 -0300

    Menu, créditos e cena de jogo criados

--- WAR/Assets/Scripts/GUI/ChangeSceneButton.cs
+++ WAR/Assets/Scripts/GUI/ChangeSceneButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuButton : MonoBehaviour {
+public class ChangeSceneButton : MonoBehaviour {
 	public string redirectSceneName;
 	public void changeSceneOnClick(){
 		SceneManager.LoadScene(redirectSceneName);
