38e0966a96b19e5d90e4d7935727a8a31a698f52
AlyssonGGS <Alyssonggs@hotmail.com>
2017-09-22 18:05:26 -0300

    Criados os componentes de clique do território e gerenciador de UI

---
+++ WAR/Assets/Script/Game/GameUIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour {
+	public Canvas canvas;
+	GameObject territoryNameGO, countTextGO;
+
+	void Start(){
+		countTextGO = GameObject.Find("Count");
+		territoryNameGO = GameObject.Find("Name");
+	}
+
+	public void updateUI(string name, int count){
+		updateTerritoryName(name);
+		updateTroopCount(count);
+	}
+	public void updateUI(int count){
+		updateTroopCount(count);
+	}
+
+	public void updateUI(string name){
+		updateTerritoryName(name);
+	}
+
+	private void updateTerritoryName(string territoryName){
+		territoryNameGO.GetComponent<Text>().text = territoryName;
+	}
+	private void updateTroopCount(int count){
+		countTextGO.GetComponent<Text>().text = count.ToString();
+	}
+}
