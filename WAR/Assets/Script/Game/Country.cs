39c667637ab776ca148d97879601608e14b17a4d
AlyssonGGS <Alyssonggs@hotmail.com>
2017-09-23 15:22:40 -0300

    mudança de nome na classe

--- WAR/Assets/Script/Game/Country.cs
+++ WAR/Assets/Script/Game/Country.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Territory : MonoBehaviour {
+public class Country : MonoBehaviour {
 
 	//mudar de acordo com a representação do jogador
 	private string player;
