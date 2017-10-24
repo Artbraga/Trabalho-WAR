e5cbf9aafdb9879668bdae5598a6c4b29e36d07d
AlyssonGGS <alyssonggs@hotmail.com>
2017-10-23 22:19:35 -0200

    Limpeza no projeto

---
+++ WAR/Assets/Script/Game/MoveCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.mousePosition.x > Screen.width *0.9f){
+			this.transform.position += Vector3.right * Time.deltaTime;
+		}
+
+		if(Input.mousePosition.x < Screen.width *0.1f){
+			this.transform.position += Vector3.left * Time.deltaTime;
+		}
+	}
+}
