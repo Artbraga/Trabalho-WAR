c3abbf45417cb04d5bb2f0a63c28e750c5634227
AlyssonGGS <alyssonggs@hotmail.com>
2017-10-24 21:59:13 -0200

    Adicionado movimento com velocidade variada

--- WAR/Assets/Script/Game/MoveCamera.cs
+++ WAR/Assets/Script/Game/MoveCamera.cs
@@ -4,40 +4,61 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour {
 
-	public Transform backgroundMap;
+	public GameObject backgroundMap;
 	Vector2 cameraSize;
+	Vector2 bgSize;
 
-	void Start(){
+    private bool moving;
+    private float speed;
+
+    void Start(){
 		cameraSize = new Vector2(getCameraWidth(),getCameraHeight());
+		bgSize = backgroundMap.GetComponent<SpriteRenderer>().bounds.size/2;
+		speed = 1;
 	}
 
 	void Update () {
+		moving = false;
 		if(	Input.mousePosition.x > Screen.width * 0.9f && 
-			this.transform.position.x + cameraSize.x/2 < backgroundMap.position.x + backgroundMap.localScale.x * 2){
-			this.transform.position += Vector3.right * Time.deltaTime;
+			this.transform.position.x + cameraSize.x < backgroundMap.transform.position.x + bgSize.x){
+				move(Vector3.right);
 		}
 
 		else if(Input.mousePosition.x < Screen.width *0.1f && 
-			this.transform.position.x - cameraSize.x/2 > backgroundMap.position.x - backgroundMap.localScale.x * 2){
-			this.transform.position += Vector3.left * Time.deltaTime;
+			this.transform.position.x - cameraSize.x > backgroundMap.transform.position.x - bgSize.x){
+				move(Vector3.left);
 		}
 
 		if(Input.mousePosition.y > Screen.height *0.9f && 
-			this.transform.position.y + cameraSize.y/2 < backgroundMap.position.y + backgroundMap.localScale.y * 4.4f){
-			this.transform.position += Vector3.up * Time.deltaTime;
+			this.transform.position.y + cameraSize.y < backgroundMap.transform.position.y + bgSize.y){
+				move(Vector3.up);
 		}
 
-		if(Input.mousePosition.y < Screen.height *0.1f && 
-			this.transform.position.y - cameraSize.y/2 > backgroundMap.position.y - backgroundMap.localScale.y * 4.4f){
-			this.transform.position += Vector3.down * Time.deltaTime;
+		else if(Input.mousePosition.y < Screen.height *0.1f && 
+			this.transform.position.y - cameraSize.y > backgroundMap.transform.position.y - bgSize.y){
+				move(Vector3.down);
 		}
+		increaseMoveSpeed();
 	}
 
 	float getCameraWidth(){
-		return this.GetComponent<Camera>().orthographicSize * this.GetComponent<Camera>().aspect;
+		return this.GetComponent<Camera>().orthographicSize  * this.GetComponent<Camera>().aspect;
 	}
 
 	float getCameraHeight(){
 		return this.GetComponent<Camera>().orthographicSize;
+	}
+
+	void move(Vector3 direction){
+		this.transform.position += direction.normalized * Time.deltaTime * speed;
+		moving = true;
+	}
+
+	void increaseMoveSpeed(){
+		if(moving){
+			speed += 0.02f;
+			return;
+		}
+		speed = 1;
 	}
 }
