32334cc9e9b052cf9b27090dccb07d8ff2bd70d2
AlyssonGGS <alyssonggs@hotmail.com>
2017-10-23 22:22:21 -0200

    Camera movendo para todos os lados

--- WAR/Assets/Script/Game/MoveCamera.cs
+++ WAR/Assets/Script/Game/MoveCamera.cs
@@ -15,8 +15,16 @@ public class MoveCamera : MonoBehaviour {
 			this.transform.position += Vector3.right * Time.deltaTime;
 		}
 
-		if(Input.mousePosition.x < Screen.width *0.1f){
+		else if(Input.mousePosition.x < Screen.width *0.1f){
 			this.transform.position += Vector3.left * Time.deltaTime;
+		}
+
+		if(Input.mousePosition.y > Screen.height *0.9f){
+			this.transform.position += Vector3.up * Time.deltaTime;
+		}
+
+		if(Input.mousePosition.y < Screen.height *0.1f){
+			this.transform.position += Vector3.down * Time.deltaTime;
 		}
 	}
 }
