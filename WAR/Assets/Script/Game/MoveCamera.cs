de1e6d2f1534f27eda0cc8876b7f0b34159e6f88
AlyssonGGS <alyssonggs@hotmail.com>
2017-10-24 21:28:29 -0200

    Camera movendo com bordas

--- WAR/Assets/Script/Game/MoveCamera.cs
+++ WAR/Assets/Script/Game/MoveCamera.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Transform backgroundMap;
+	Vector2 cameraSize;
+
+	void Start(){
+		cameraSize = new Vector2(getCameraWidth(),getCameraHeight());
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if(Input.mousePosition.x > Screen.width *0.9f){
+		if(	Input.mousePosition.x > Screen.width * 0.9f && 
+			this.transform.position.x + cameraSize.x/2 < backgroundMap.position.x + backgroundMap.localScale.x * 2){
 			this.transform.position += Vector3.right * Time.deltaTime;
 		}
 
-		else if(Input.mousePosition.x < Screen.width *0.1f){
+		else if(Input.mousePosition.x < Screen.width *0.1f && 
+			this.transform.position.x - cameraSize.x/2 > backgroundMap.position.x - backgroundMap.localScale.x * 2){
 			this.transform.position += Vector3.left * Time.deltaTime;
 		}
 
-		if(Input.mousePosition.y > Screen.height *0.9f){
+		if(Input.mousePosition.y > Screen.height *0.9f && 
+			this.transform.position.y + cameraSize.y/2 < backgroundMap.position.y + backgroundMap.localScale.y * 4.4f){
 			this.transform.position += Vector3.up * Time.deltaTime;
 		}
 
-		if(Input.mousePosition.y < Screen.height *0.1f){
+		if(Input.mousePosition.y < Screen.height *0.1f && 
+			this.transform.position.y - cameraSize.y/2 > backgroundMap.position.y - backgroundMap.localScale.y * 4.4f){
 			this.transform.position += Vector3.down * Time.deltaTime;
 		}
+	}
+
+	float getCameraWidth(){
+		return this.GetComponent<Camera>().orthographicSize * this.GetComponent<Camera>().aspect;
+	}
+
+	float getCameraHeight(){
+		return this.GetComponent<Camera>().orthographicSize;
 	}
 }
