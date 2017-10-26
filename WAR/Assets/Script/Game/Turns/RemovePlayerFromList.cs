a6ce26bc6021b0047bfe525555d9c90aefa3d9af
AlyssonGGS <alyssonggs@hotmail.com>
2017-10-26 00:36:21 -0200

    Script para remoção de player

---
+++ WAR/Assets/Script/Game/Turns/RemovePlayerFromList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovePlayerFromList : MonoBehaviour {
+
+	public void remove(){
+		this.transform.parent.parent = null;
+		reorganizeChilds();
+		destroyMe();
+	}
+
+    private void reorganizeChilds(){
+		GameObject playerList = GameObject.Find("PlayersList");
+		playerList.GetComponent<PlayerList>().reorganizeList();
+    }
+
+    private void destroyMe(){
+		Destroy(this.transform.parent.gameObject);
+	}
+}
