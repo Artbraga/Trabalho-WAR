1725a596003e940f51376f72855f27c03e9add6f
AlyssonGGS <alyssonggs@hotmail.com>
2017-11-24 11:38:22 -0200

    Criada classe de gerencia dos territorios

---
+++ WAR/Assets/Script/CountryList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountryList{
+	private static CountryList instance = new CountryList();
+	Dictionary<string,Country> countries;
+	private CountryList(){
+		GameObject[] countriesGO = GameObject.FindGameObjectsWithTag("territories");
+		foreach(GameObject go in countriesGO){
+			countries.Add(go.name, go.GetComponent<Country>());
+		}
+	}
+
+	public static CountryList getInstance(){
+		return instance;
+	}
+
+	public Country getCountry(string countryName){
+		return countries[countryName];
+	}
+
+	public void setTroopToCountry(string countryName, short count){
+		getCountry(countryName).TroopsCount = count;
+	}
+}
