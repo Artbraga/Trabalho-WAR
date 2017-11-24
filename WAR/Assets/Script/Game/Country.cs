ab530eda1ed62d7b4f73feaed5f288e2d2124bee
AlyssonGGS <alyssonggs@hotmail.com>
2017-11-24 11:39:39 -0200

    Criação do array de vizinhos

--- WAR/Assets/Script/Game/Country.cs
+++ WAR/Assets/Script/Game/Country.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 public class Country : MonoBehaviour {
 
 	//mudar de acordo com a representação do jogador
-	private string player;
-	public string Player{
-		get{
-			return this.player;
-		}
-		set{
-			this.player = value;
-			this.gameObject.name =  value;
-		}
-	}
-
+	public string[] neighboors;
 	private short troopsCount;
 	public short TroopsCount{
 		get{
@@ -33,8 +23,9 @@ public class Country : MonoBehaviour {
 
 	void Start(){
 		TroopsCount = (short)Random.Range(0,5);
-		Player = this.gameObject.name;
+		// Player = this.gameObject.name;
 	}
+	
 	void OnMouseDown(){
 		showTerritoryInfo();
 	}	
@@ -43,6 +34,6 @@ public class Country : MonoBehaviour {
 		//chamar GUI
 		//atualizar informações da gui
 		//mudar estado da GUI
-		Camera.main.GetComponent<GameUIController>().updateUI(Player,TroopsCount);
+		// Camera.main.GetComponent<GameUIController>().updateUI(Player,TroopsCount);
 	}
 }
