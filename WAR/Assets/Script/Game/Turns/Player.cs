57dd14a8df8b03189cedd438882daca3b348317c
AlyssonGGS <alyssonggs@hotmail.com>
2017-11-24 11:42:38 -0200

    Controle de turnos e adicionada a informação de territorios no player

---
+++ WAR/Assets/Script/Game/Turns/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player{
+
+	private short id;
+	private Color color;
+	private List<Country> conqueredTerritories;
+	public Color Color { 
+		get{ return this.color; }
+	}
+
+	public Player(short id, Color color){
+		this.id = id;
+		this.color = color;
+	}
+
+	public void addCountry(Country country){
+		conqueredTerritories.Add(country);
+	}
+
+	public void removeTerritory(Country country){
+		conqueredTerritories.Remove(C)
+	}
+}
---
+++ WAR/Assets/Script/Game/Turns/TurnController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnController : MonoBehaviour {
+
+	private Queue<Player> players;
+	private long turn;
+	private short playerCount;
+	void Start () {
+		this.playerCount = (short)players.Count;
+		turn = 0;
+	}
+	
+	void Update () {
+	}
+
+	public void nextPlayerTurn(){
+		players.Enqueue(players.Dequeue());
+	}
+
+	public Queue<Player> getPlayers(){
+		return players;
+	}
+
+	public Player getActualPlayer(){
+		return players.Peek();
+	}
+}
