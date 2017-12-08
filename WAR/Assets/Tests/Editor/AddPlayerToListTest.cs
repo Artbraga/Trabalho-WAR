48e3de8f88dec91f310fd8303067c3b5a2f198b8
Matheus <connect.malves@gmail.com>
2017-12-07 23:33:49 -0200

    Testes

---
+++ WAR/Assets/Tests/Editor/AddPlayerToListTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class AddPlayerToListTest {
+
+	[Test]
+	public void AddPlayerToListTestSimplePasses() {
+        // Use the Assert class to test conditions.
+        GameObject playerPrefab = Resources.Load("Prefabs/player.prefab") as GameObject;
+        GameObject playerList = new GameObject();
+        playerList.AddComponent(typeof(AddPlayerToList));
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+	public IEnumerator AddPlayerToListTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
